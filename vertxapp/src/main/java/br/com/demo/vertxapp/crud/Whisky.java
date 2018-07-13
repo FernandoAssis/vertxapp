6f681d3c116bb6ea1ac5056c3fef3ec4c7bcd400
FernandoAssis <fernando.uplex@gmail.com>
2018-07-13 17:06:45 -0300

    versao inicial do crud

---
+++ vertxapp/src/main/java/br/com/demo/vertxapp/crud/Whisky.java
@@ -0,0 +1,50 @@
+package br.com.demo.vertxapp.crud;
+
+import java.io.Serializable;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Whisky implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private static final AtomicInteger COUNTER = new AtomicInteger();
+
+	  private final int id;
+
+	  private String name;
+
+	  private String origin;
+
+	  public Whisky(String name, String origin) {
+	    this.id = COUNTER.getAndIncrement();
+	    this.name = name;
+	    this.origin = origin;
+	  }
+
+	  public Whisky() {
+	    this.id = COUNTER.getAndIncrement();
+	  }
+
+	  public String getName() {
+	    return name;
+	  }
+
+	  public String getOrigin() {
+	    return origin;
+	  }
+
+	  public int getId() {
+	    return id;
+	  }
+
+	  public void setName(String name) {
+	    this.name = name;
+	  }
+
+	  public void setOrigin(String origin) {
+	    this.origin = origin;
+	  }
+}
